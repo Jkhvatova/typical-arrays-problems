e599582d8bded0973d0cbca788ec9d17720db3d6
Julia Khvatova <jkhvatova@gmail.com>
2020-10-03 00:10:42 +0300

    feat: task solved

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 
 exports.min = function min (array) {
-  return 0;
+  if  (array === undefined || array.length === 0) {
+    return 0;
+  } else {
+    let minNum = array[0];
+    for (var i = 0; i < array.length; i++) {
+      if (minNum > array[i]) {
+        minNum = array[i];
+    } 
+  }
+return minNum;  
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array === undefined || array.length === 0) {
+    return 0;
+  }
+  let maxNum = array[0];
+  for (var i = 0; i < array.length; i++) {
+    if (maxNum < array[i]) {
+      maxNum = array[i];
+    } 
+  }
+  return maxNum;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined || array.length === 0) {
+    return 0;
+  }
+  let sum = 0;
+  for (var i = 0; i < array.length; i++) {
+    sum += array[i]; 
+  }
+  let avg = sum / array.length;
+  return avg;
 }
